cf39ae393a96757a1d665e97fe372e39c7816272
giacintapopolizio <giacinta.popolizio@mail.polimi.it>
2025-01-25 10:34:19 +0100

    legenda e scritta

--- sun/sketchsun.js
+++ sun/sketchsun.js
@@ -132,6 +132,13 @@ function draw() {
     text(stringhetta, xPos - 13/16*xPos, (totalHeight/2 - 8/50*size));
     }
     else {
+      fill(255, 255, 191, 70);
+      noStroke();
+      textFont(RegularBaskerville);
+      textSize (size *0.04)
+      textAlign(LEFT, TOP);
+      let scrittaInizio = "Hover with your mouse to view the parameters._Click to learn more about each one.".replace(/_/g, '\n');
+      text(scrittaInizio, xPos - 13/16*xPos, (totalHeight/2 - 8/50*size))
       cursor("default");
     }
   
@@ -147,7 +154,7 @@ function draw() {
    let scaleFactor = 0.00155*size;
    let scaledWidth = legendaUno.width * scaleFactor;
    let scaledHeight = legendaUno.height * scaleFactor;
-   image(legendaUno, xPos-13/16*xPos, 1/2*windowHeight + 2/50*size , scaledWidth, scaledHeight);
+   image(legendaUno, xPos-13/16*xPos, windowHeight - 21.6/16*legendaUno.height, scaledWidth, scaledHeight);
  
    // ----------------------- testi curvi ---------------------------------
    testoCurvoEconomic (xPos, yPos, size);
